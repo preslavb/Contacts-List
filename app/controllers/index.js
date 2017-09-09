d3805ed8f231a75edd5fff701c941c506e0da8da
preslavb <preslavb@abv.bg>
2017-09-09 16:00:08 +0100

    Unique records implementation
    Only add records if one doesn't already exist in the model.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,12 +4,36 @@ export default Ember.Controller.extend({
   //Create a Store record of a new contact using the values from the form
   actions: {
     submitFormData(firstName, surname, phoneNum, mobileNum) {
-      this.store.createRecord('contact', {
+      //Get all of the contacts in model
+      let recordsToCompare = this.store.peekAll('contact');
+      //Bool tracking whether a match was found
+      let recordAlreadyInModel = false;
+
+      //Only check the records for a match if there are any records in the model
+      if(recordsToCompare.get('length') != 0)  {
+        //Loop through all records and compare them to the one being added. If it is a complete match, then set the tracking bool to true
+        for (var i = 0 ; i < recordsToCompare.get('length'); i++) {          
+          let recordToCompare = recordsToCompare.objectAt(i);
+
+          if (recordToCompare.get('firstName') == firstName && recordToCompare.get('surname') == surname && 
+              recordToCompare.get('phone') == phoneNum && recordToCompare.get('mobile') == mobileNum) {
+            recordAlreadyInModel = true;
+          }
+        }
+      }
+
+      //If there were no matches found, add the contact to the model
+      if(!recordAlreadyInModel)  {
+        this.store.createRecord('contact', {
         firstName: firstName,
         surname: surname,
         phone: phoneNum,
         mobile: mobileNum
-      })
+        });
+      }
+      else  {
+        alert("Record already in contacts");
+      }
     },
 
     deleteContact(contactID)  {
